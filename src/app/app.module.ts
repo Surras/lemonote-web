da4495ed73783507255c0eb9b12c788dbeeaba51
mrehmer <marco.rehmer@ironic-dev.de>
2019-12-11 23:45:04 +0100

    feat: add some basic compoenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    ContentListComponent
   ],
   imports: [
     BrowserModule
